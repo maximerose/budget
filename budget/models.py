4aa15e596e8797bf6b03a55f0468c6ee8fcf8f2d
maximerose <maxime.r80@gmail.com>
2026-08-18 16:21:16 +0200

    feat(budget): add BankAccount model

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.db import models
 
@@ -29,3 +31,34 @@ class Category(BaseModel, SoftDeleteModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class AccountType(models.TextChoices):
+    CHECKING = "CHECKING", "Compte courant"
+    SAVINGS = "SAVINGS", "Compte épargne"
+    BUSINESS = "BUSINESS", "Compte pro"
+    MEAL_VOUCHER = "MEAL_VOUCHER", "Tickets resto"
+    OTHER = "OTHER", "Autre"
+
+
+class BankAccount(BaseModel, SoftDeleteModel):
+    name = models.CharField(max_length=100, null=False)
+    account_type = models.CharField(
+        max_length=20, choices=AccountType.choices, default=AccountType.CHECKING
+    )
+    owner = models.ForeignKey(
+        HouseholdMember,
+        on_delete=models.CASCADE,
+        related_name="bank_accounts",
+    )
+    current_balance = models.DecimalField(
+        max_digits=15, decimal_places=2, default=Decimal("0.0")
+    )
+
+    class Meta(BaseModel.Meta, SoftDeleteModel.Meta):
+        verbose_name = "Compte bancaire"
+        verbose_name_plural = "Comptes bancaires"
+
+    def __str__(self) -> str:
+        account_type_label = AccountType(self.account_type).label
+        return f"{self.name} ({account_type_label})"
